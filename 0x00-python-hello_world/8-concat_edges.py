72b2ff70145e8ac666d3ae48c594a4699e4b09c0
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-02 13:56:22 -0800

    my first

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+str = python is interpreted interactive, object-oriented programing\
+        language that combine remarkable power with very clear syntax"
+        str = str[39:67] + str[107 + 112] + str[:6]
+        print(str)
