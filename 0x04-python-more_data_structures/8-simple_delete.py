c5587d632f8fa4b741561a356ebf2fa19eb4c385
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-10 11:28:14 -0800

    my first

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 # 8-simple_delete.py
 
 
-def simple_delete(a_dictionary, key=""):
+def simple_delete(""a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
         return (a_dictionary)
