6a8754baa14df93cd3f6988615f6d6593821c8cf
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-13 11:44:59 -0800

    my first

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,10 +4,9 @@
 """print all possible different combinations of tow digits ascending order."""
 
 the tow digit must be different - 01 and 10 are considered identical.
-"""
 for digit in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
     if digit1 == 8 and digit2 == 9:
     print("{}{}".format(digit1, digit2))
     else:
-    print("{}{}".format(digit1, digit2), end=" ")
+    print("{}{}".format(digit1, digit2), end=", ")
