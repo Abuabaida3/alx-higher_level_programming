9a34c699b963dfc6205f68419c849a7c10d84731
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-13 11:52:09 -0800

    my first

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 7-islower.py
+
+
+def islower(c):
+    """check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return true
+    else:
+        return false
