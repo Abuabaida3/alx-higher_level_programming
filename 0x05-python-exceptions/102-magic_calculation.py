d1f7f042e05674711e2c7e5e4f7c64954afe9b71
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-22 11:05:32 -0800

    myfirst

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 102-Magic_calculation.py
+
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('too far')
+            else:
+                result += a ** b / i
+        except:
+            rsult = b + a
+            break
+        return (result)
+
