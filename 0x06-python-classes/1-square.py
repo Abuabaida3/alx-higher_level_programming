3abdc041c254c81fdd69b48f7d9a7fd68923de71
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-24 10:52:01 -0800

    my first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def_init_(self, size):
+        """Initialize a new Square.
+
+        Args:
+        size (int): the size of the new square.
+        """
+        self._size = size
