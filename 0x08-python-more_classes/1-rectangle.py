89fb155b954302a0b32135b9ba117d8f12fde091
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-29 08:38:58 -0800

    my first

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# 1-ectangle.py
+"""Define a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a Rectangle.
+
+    Args:
+      width (int): The width of the new rectangle.
+      heigh (int): The heigh of the new rectangle.
+      """
+      self.width = width
+      self.heigh = heigh
+
+      @property
+      def width(self):
+          """Get/set the width of the rectangle."""
+          return self.__width
+
+      @width.setter
+      def with(self,value):
+          raise TypeError("width must be an integer")
+      if value < 0:
+          raise ValueError("width must be >= 0")
+      self.__width = value
+
+      @property
+      def height(self):
+          """Get/set the height of the rectangle."""
+          return self.__height
+
+      def heigh(self):
+          if not isinstance(value, int):
+              raise TypeError("meight must be an integer")
+          if value < 0:
+              raise ValueError(Height must be >= 0")
+              self.__height = value
