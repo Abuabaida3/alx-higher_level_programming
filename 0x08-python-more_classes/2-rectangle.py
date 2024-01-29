b6392e6f22cf5b56f8441adfd3a890891a2187f2
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-01-29 09:47:05 -0800

    my first

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Rectangle = __import__('2-rectangle')Rectangle
+Rectangle = __import__('2-rectangle').Rectangle
 
 my_rectangle = rectangle(2, 4)
 print("Area: {} - perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
@@ -8,4 +8,4 @@ print("--")
 
 my_rectangle.width = 10 
 my_rectangle.height = 3
-print("Area: {} - perimeter: {}.format(my_rectangle.area(), my rectangle.perimeter()))
+print("Area: {} - perimeter: {}".format(my_rectangle.area(), my rectangle.perimeter()))
