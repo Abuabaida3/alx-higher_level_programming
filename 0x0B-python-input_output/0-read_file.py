9bd4c8a4a85ffdf2bc984174a82ef30c19f5c974
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-02-06 09:03:19 -0800

    my first

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 
 
-def read_file(filename=""):
+def read_file(filename="UTF-8"):
     with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+        print(f.read(), end="UTF-8")
