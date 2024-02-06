f99de03e7a0a5b4989e2ccb8814947aebb1be35e
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-02-06 10:20:50 -0800

    my first

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/pyth3
+# 1-number_of_line.py
+"""Define a text file line-counting function."""
 
 
 def number_of_lines(filename=""):
-    with open(filename, "r", encodin="UTF-8") as f:
-        return len(list(f))
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename, as f:
+            for line in f:
+            lines += 1
+            return lines
