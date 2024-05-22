43021aee81d7ad4b3f2f0607af5eb4e4a0be6160
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-05-22 14:33:35 -0700

    my first

--- 0x0D-SQL_introduction/0-privileges.sql
+++ 0x0D-SQL_introduction/0-privileges.sql
@@ -1,3 +1,3 @@
 1 -- list all privileges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR  'user_0d_2'@'localhost';
+2 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+3 SHOW GRANTS FOR  'user_0d_2'@'localhost';
