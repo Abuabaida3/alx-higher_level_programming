7198b8976b39d6fc966a4fddccf6742328189d58
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-18 23:24:09 +0000

    my first

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!usr/bin/node
+const arg = process.argv[2];
+if (arg === undefined){
+console.log('no argument');
+}else{
+console.log(arg);
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const args = ['C is fun', 'python is cool', 'javaScript is amazing'];
+for (const arg in args){
+console.log(args[arg]);
+}
