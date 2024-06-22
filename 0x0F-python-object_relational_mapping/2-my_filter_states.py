27c0e1068d7b441a533f2338c74ec08fe36e29fa
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-22 15:35:07 -0700

    my first

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, cursor = db.cursor()
+
+            query = "SELECT * FOROM states WHERE name='{}' ORDER BY id ASC".format(state_name)
+            for row in cursor.fetchall()
+            print(row)
+            cursor.close()
+            db.close
