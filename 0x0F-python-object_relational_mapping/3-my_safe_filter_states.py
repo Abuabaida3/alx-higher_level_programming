ee6e2014aaac91cb2b22c273fdb5d4b7d386ec00
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-23 13:57:04 -0700

    my first

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Scrpt that in an argument
+"""
+import Mysqldb
+from sys import argv
+
+if __name__ '__main__':
+    db = MYSQLdb.connect(host="localhost", port=3306, user=arg[1], passwd=argv[2], db=argv[3]
+            cur = db.cursor()
+            cur.excute("SELECT * form states WHERE BINARY name = %s, [argv]])
+
+            row = cur.fetechall
+            for i in row:
+            print(i)
+            #Clean up process
+            cur.close()
+            db.close()
