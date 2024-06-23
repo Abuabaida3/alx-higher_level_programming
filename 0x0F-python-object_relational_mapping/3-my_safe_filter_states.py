a756ac74c2e23a9c7d0c11ac0800eff7c5cdf4f4
Abuabaida3 <abuabaida.ahmed@gmail.com>
2024-06-23 14:18:42 -0700

    my first

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,7 @@ Scrpt that in an argument
 import Mysqldb
 from sys import argv
 
-if __name__ '__main__':
+if __name__ == '__main__':
     db = MYSQLdb.connect(host="localhost", port=3306, user=arg[1], passwd=argv[2], db=argv[3]
             cur = db.cursor()
             cur.excute("SELECT * form states WHERE BINARY name = %s, [argv]])
